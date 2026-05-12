b363abb9da58089ad0fdc33d52cf5a36bf3884fd
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-05-12 10:47:30 +0600

    crud operation create user is added

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,6 +1,7 @@
+import { AddUserModal } from "@/components/AddUser";
 import { usersData } from "@/components/Data"
 import UsersCard from "@/components/UsersCard";
-import { deleteUser } from "@/lib/actions";
+import { createUser, deleteUser } from "@/lib/actions";
 
 export default async function UsersPage() {
     const users = await usersData();
@@ -8,6 +9,7 @@ export default async function UsersPage() {
     <div>
         <h1 className="text-5xl text-center font-bold pt-10">This is Users Page</h1>
         <h3 className="text-3xl text-center font-bold pt-4">Users : {users.length}</h3>
+        <div className="flex justify-center items-center mt-3"><AddUserModal createUser={createUser}/></div>
         <div className="max-w-7xl mx-auto grid grid-cols-4 gap-4 mt-5">
           {
             users.map(user => <UsersCard key={user._id} user={user} deleteUser={deleteUser}/>)
--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,15 +1,35 @@
 import { revalidatePath } from "next/cache";
 
+export const createUser = async (formData) => {
+  "use server";
+  const newUser = Object.fromEntries(formData.entries());
+
+  const res = await fetch("http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  });
+  const result = await res.json();
+
+  if (result.insertedId) {
+    revalidatePath("/users");
+  }
+
+  return result;
+};
+
 export const deleteUser = async (userId) => {
   "use server";
   const res = await fetch(`http://localhost:8000/users/${userId}`, {
     method: "DELETE",
   });
   const data = await res.json();
-  console.log(data)
+  console.log(data);
 
-  if(data.deletedCount > 0) {
-    revalidatePath("/users")
+  if (data.deletedCount > 0) {
+    revalidatePath("/users");
   }
 
   return data;
